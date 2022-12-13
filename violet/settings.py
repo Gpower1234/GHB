1e4ec85a7a1d74f267a54683287e19d8aa1486f0
gpower12345 <63840160+gpower12345@users.noreply.github.com>
2022-12-13 10:57:35 +0100

    updated

--- violet/settings.py
+++ violet/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-CSRF_TRUSTED_ORIGINS = ['https://*.goodnesshairbraids']
+CSRF_TRUSTED_ORIGINS = ['https://*.goodnesshairbraids.com']
 
 ALLOWED_HOSTS = ['*']
 
